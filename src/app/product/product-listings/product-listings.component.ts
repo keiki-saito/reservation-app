c47cab62a0f63b801ac8c69daa20b50992ea5b4c
keiki-saito <keicake0808@gmail.com>
2023-06-21 21:04:12 +0900

    Angularフロントエンド側作成

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  products: any;
+
+  ngOnInit(): void {
+    this.products = products;
+  }
+}
---
+++ src/app/products.ts
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingText1:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText2:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText3:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    coverImage: './assets/img/phone-cover.jpeg',
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingText1:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText2:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText3:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    coverImage: './assets/img/phone-cover.jpeg',
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingText1:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText2:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText3:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    coverImage: './assets/img/phone-cover.jpeg',
+  },
+  {
+    name: 'Phone Special',
+    price: 999,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingText1:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText2:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    headingText3:
+      'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    coverImage: './assets/img/phone-cover.jpeg',
+  },
+];
